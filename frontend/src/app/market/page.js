08d7f342a7edf47ae0a311204dbdd79bcfb20445
dongrep <dongre.p@northeastern.edu>
2024-11-09 21:30:57 -0500

    testing create option

--- frontend/src/app/market/page.js
+++ frontend/src/app/market/page.js
@@ -1,20 +1,63 @@
 //! Maybe not use client when we get the apis
 "use client";
 
-import { useState } from "react";
+import { useContext, useState } from "react";
 import MarketCard from "../components/MarketCard";
 import { useMarket } from "../MarketContext";
-import MarketChart from "../components/MarketChart";
 import MarketDetailsPage from "../components/MarketDetailsPage";
-
-// export const metadata = {
-//   title: "BitBet",
-//   description: "A decentralized prediction market platform built on Stacks",
-// };
+import { UserContext } from "../UserContext";
+import {
+  broadcastTransaction,
+  FungibleConditionCode,
+  makeContractCall,
+  makeStandardSTXPostCondition,
+  uintCV,
+} from "@stacks/transactions";
+import { StacksTestnet } from "@stacks/network";
 
 export default function Market() {
   const { markets } = useMarket();
   const [selectedMarket, setSelectedMarket] = useState(null);
+  const { userData } = useContext(UserContext);
+
+  const createMarket = async () => {
+    if (!userData.profile.stxAddress.testnet) {
+      alert("Please connect your wallet to create a market.");
+      return;
+    }
+    const senderKey = userData.appPrivateKey;
+    const stacksAddress = userData.profile.stxAddress.testnet;
+
+    const txOptions = {
+      contractAddress: "ST1ZGCN5D7C3MZZY4GC31F8ANDD5VHS5FQ7HEKNQR",
+      contractName: "fair-turquoise-ferret",
+      functionName: "create-option",
+      functionArgs: [uintCV(1), uintCV(100000)], // Adjust these values as needed
+      senderKey: senderKey,
+      network: new StacksTestnet(),
+      fee: 3000, // Increase fee in microSTX
+      postConditions: [
+        makeStandardSTXPostCondition(
+          stacksAddress,
+          FungibleConditionCode.LessEqual,
+          1500, // Adjust post condition to match your token amount
+        ),
+      ],
+    };
+
+    try {
+      const transaction = await makeContractCall(txOptions);
+      const result = await broadcastTransaction(
+        transaction,
+        new StacksTestnet(),
+      );
+      console.log(result);
+      alert("Market created successfully! " + result.txid);
+    } catch (error) {
+      console.error(error);
+      alert("Failed to create market: " + error.message);
+    }
+  };
 
   return (
     <div className="flex w-full h-full">
@@ -22,6 +65,11 @@ export default function Market() {
         <MarketDetailsPage market={selectedMarket} />
       ) : (
         <div className="flex-col pb-5">
+          <button
+            onClick={createMarket}
+            className="mt-4 ml-4 text-white bg-gray-800 rounded px-4 py-2">
+            Create Market
+          </button>
           <h1 className="mt-8 text-4xl font-semibold text-black text-center">
             BitBet
           </h1>
